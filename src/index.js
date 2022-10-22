a96d8a7e1c1275a9aa2259464aafdf8af67fc036
Mitali2700 <Mitali2700>
2022-10-22 23:05:47 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />  {/* app component jab bhi ise likhenge tab tab app.js wala */}
+    <App />
+    <App />
+    <App />
   </React.StrictMode>
 );
 
