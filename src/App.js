19a60c47191425efd77904bafd1960fb821f7702
Mitali2700 <Mitali2700>
2022-10-23 21:24:29 +0530

    passing object

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 
 
+const stylingOptions ={
+  backgroundColor: "red"
+}
+
 function App() {
   return (
     <div>
+    <div style={stylingOptions}>
       img
       const
-      <div className='class' id='id'>
-    {/* <label htmlFor=''></label> */}
-        {/* demo1 */}
+      price
+      {/* <div className='class' id='id'> */}
+      {/* <label htmlFor=''></label> */}
+      {/* demo1 */}
+      </div>
+
+      <div>
+        hello world
       </div>
       
-      price
-    </div>
+    </div> 
+    
+    
     // It is not HTML. It is JSX (Jvascript XML).
-
+    
   );
 }
 
