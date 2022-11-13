23b7c50ba11bc3ecfe7f620d8cd86bb3ff7495bb
Mitali2700 <Mitali2700>
2022-11-13 23:28:22 +0530

    data.json added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import './App.css';
 
 
-const stylingOptions ={
-  backgroundColor: "red",
-  padding: "10px"
-}
-
 function App() {
+ 
   return (
-    <>
-    <div style={stylingOptions}>
-      img
-      const
-      price
-      {/* <div className='class' id='id'> */}
-      {/* <label htmlFor=''></label> */}
-      {/* demo1 */}
-      </div>
+    <div>
 
-      <div style={{backgroundColor:"blue", padding: "20px"}}>
-        hello world
-      </div>
-
-    </> 
-    
-    
+    </div>
     // It is not HTML. It is JSX (Jvascript XML).
     
   );
 }
 
 export default App;
+
+
+/* If we write: 
+import Nik from './navbar';
+
+function App(){
+  return(
+    <>
+    <Nik>
+  )
+}
+
+export default App; // navbar.js page
+
+It will also return back NAVBAR on webpage bcz we are importing somethong from './navbar'.
+So it will return NAVBAR by default. Because we are exporting using default variable. 
+
+
+But what happen If we remove default from export ?
+code:   export {Navbar}// navbar.js page
+
+It will give error: doesnot contain a dfault export.
+*/
